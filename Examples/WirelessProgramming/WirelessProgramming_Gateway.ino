3c1d4264e5a16f5fec193a16d0a53e48465229eb
LowPowerLab <flexiboiu@gmail.com>
2013-05-04 23:50:48 -0300

    Update WirelessProgramming_Gateway.ino

--- Examples/WirelessProgramming/WirelessProgramming_Gateway.ino
+++ Examples/WirelessProgramming/WirelessProgramming_Gateway.ino
@@ -26,7 +26,7 @@
 #define TARGET_ID         55      //ID of node being wirelessly reprogrammed
 #define GROUPID          100
 #define SERIAL_BAUD   115200
-#define ACK_TIME         100  // # of ms to wait for an ack
+#define ACK_TIME          20  // # of ms to wait for an ack
 #define TIMEOUT         3000
 
 RFM12B radio;
@@ -44,20 +44,12 @@ void setup(){
 void loop(){
   byte inputLen = readSerialLine(input);
   
-  if (inputLen == 1) {
-    c = Serial.read();
-    if (c == 'f') //f=transmit flash to node
-    {
-      sprintf(buffer, "Sending FLASH to NODE %d", TARGET_ID);
-      Serial.println(buffer);
-      HandleOutgoingFlashData();
-    }
-  }
-  else if (inputLen == 4 && input[0]=='F' && input[1]=='L' && input[2]=='X' && input[3]=='?') {
-    CheckForSerialHEX((byte*)input, inputLen, radio, TARGET_ID, TIMEOUT, ACK_TIME, true);
+  if (inputLen == 4 && input[0]=='F' && input[1]=='L' && input[2]=='X' && input[3]=='?') {
+    CheckForSerialHEX((byte*)input, inputLen, radio, TARGET_ID, TIMEOUT, ACK_TIME, false);
   }
   else if (inputLen>0) { //just echo back
-    Serial.print("RECEIVED > ");Serial.println(input);
+    Serial.print("SERIAL IN > ");Serial.println(input);
+    //Serial.print(", ");for (byte x =0;x<inputLen;x++) Serial.print(input[x], HEX);
   }
   
   if (radio.ReceiveComplete())
@@ -76,77 +68,5 @@ void loop(){
     else Serial.print("BAD-CRC");
     
     Serial.println();
-  }
-}
-
-
-//handle transmission of a FLASH image
-void HandleOutgoingFlashData()
-{
-  Serial.println();
-  int seq=0;
-  int tmp,len;
-  long now=millis();
-
-  while(1){
-    if (seq==0)
-    {
-      //begin handshake
-      radio.Send(TARGET_ID, "FLX?", 4, true);
-      Serial.println("FLX?");
-    }
-
-    if (seq >=1 && seq<=4)
-    {
-      uint8_t b = sprintf(buffer, "FLX:%d:", seq);
-      for(byte i=0;i<24;i++)
-        buffer[b+i] = FLASH[(seq-1)*24+i];
-      
-      for(byte i=0;i<24+b;i++)
-        Serial.print((char)buffer[i]);
-      Serial.println();
-
-      radio.Send(TARGET_ID, buffer, 24+b, true);
-    }
-
-    if (seq==-1)
-    {
-      radio.Send(TARGET_ID, "FLX?EOF", 7, true);
-      Serial.println("FLX?EOF");
-    }
-
-    if (waitForAck(radio, ACK_TIME))
-    {
-      tmp = *radio.DataLen;
-      for(byte i=0;i<tmp;i++)
-        Serial.print((char)radio.Data[i]);
-      Serial.println();
-      
-      if (tmp >= 4 && radio.Data[0]=='F' && radio.Data[1]=='L' && radio.Data[2]=='X')
-      {
-        now = millis();
-        if (radio.Data[3]==':')
-        {
-          sscanf((const char*)radio.Data, "FLX:%d:OK", &tmp);
-          if (tmp==4) seq=-1;
-          else if (tmp == seq) seq++;
-        }
-        if (radio.Data[3]=='?' && radio.Data[4]=='O' && radio.Data[5]=='K')
-        {
-          if (seq==-1)
-          {
-            Serial.println("FINISHED!");
-            break;
-          }
-          if (seq==0) seq++;
-        }
-      }
-    }
-
-    if (millis()-now > TIMEOUT)
-    {
-      Serial.println("Timeout, aborting FLASH operation ...");
-      break; //abort FLASH sequence if no valid ACK was received for a long time
-    }
   }
 }
