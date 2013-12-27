de7e409c1609ea0389787b3097b4fa10ed54fca9
Felix Rusu <felix@lowpowerlab.com>
2013-12-27 16:57:51 -0500

    Add targeted ID wireless programming
    Specify the node ID of the target in the command line by newly added
    command line argument
    Some refactoring

--- Examples/WirelessProgramming_gateway/WirelessProgramming_gateway.ino
+++ Examples/WirelessProgramming_gateway/WirelessProgramming_gateway.ino
@@ -22,38 +22,65 @@
 #include <SPIFlash.h>
 #include <WirelessHEX.h>
 
-#define MYID        1       // node ID used for this unit
-#define TARGET_ID   55      //ID of node being wirelessly reprogrammed
-#define GROUPID     100
+#define NETWORKID          100  //what network this node is on
+#define NODEID               5  //this node's ID, should be unique among nodes on this NETWORKID
 //Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 //#define FREQUENCY   RF12_433MHZ
 //#define FREQUENCY   RF12_868MHZ
 #define FREQUENCY   RF12_915MHZ
+
 #define SERIAL_BAUD 115200
 #define ACK_TIME    20  // # of ms to wait for an ack
 #define TIMEOUT     3000
+#define LED         9
 
 RFM12B radio;
 char c = 0;
 char FLASH[97] = ":1234567890ABCDEFGHIJKLM:ABCDEFGHIJKLM1234567890:1234567890AAAAAABBBBBBB:CCCCCCDDDDDDD1234567890";
-char buffer[256];
 char input[64]; //serial input buffer
+byte targetID=0;
 
 void setup(){
   Serial.begin(SERIAL_BAUD);
-  radio.Initialize(MYID, FREQUENCY, GROUPID);
+  radio.Initialize(NODEID, FREQUENCY, NETWORKID);
   Serial.print("Start...");
 }
 
 void loop(){
-  byte inputLen = readSerialLine(input);
+  byte inputLen = readSerialLine(input, 10, 64, 100); //readSerialLine(char* input, char endOfLineChar=10, byte maxLength=64, uint16_t timeout=1000);
   
   if (inputLen == 4 && input[0]=='F' && input[1]=='L' && input[2]=='X' && input[3]=='?') {
-    CheckForSerialHEX((byte*)input, inputLen, radio, TARGET_ID, TIMEOUT, ACK_TIME, false);
+    if (targetID==0)
+      Serial.println("TO?");
+    else
+    CheckForSerialHEX((byte*)input, inputLen, radio, targetID, TIMEOUT, ACK_TIME, false);
+  }
+  else if (inputLen>3 && inputLen<=6 && input[0]=='T' && input[1]=='O' && input[2]==':')
+  {
+    byte newTarget=0;
+    for (byte i = 3; i<inputLen; i++) //up to 3 characters for target ID
+      if (input[i] >=48 && input[i]<=57)
+        newTarget = newTarget*10+input[i]-48;
+      else
+      {
+        newTarget=0;
+        break;
+      }
+    if (newTarget>0)
+    {
+      targetID = newTarget;
+      Serial.print("TO:");
+      Serial.print(newTarget);
+      Serial.println(":OK");
+    }
+    else
+    {
+      Serial.print(input);
+      Serial.print(":INV");
+    }
   }
   else if (inputLen>0) { //just echo back
     Serial.print("SERIAL IN > ");Serial.println(input);
-    //Serial.print(", ");for (byte x =0;x<inputLen;x++) Serial.print(input[x], HEX);
   }
   
   if (radio.ReceiveComplete())
@@ -73,4 +100,13 @@ void loop(){
     
     Serial.println();
   }
+  Blink(LED,5); //heartbeat
+}
+
+void Blink(byte PIN, int DELAY_MS)
+{
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
 }
